84ee115f88c03ffe56e5718de37380ebe9e9092c
caojun <fourmi_103683@hotmail.com>
2018-10-08 16:04:06 +0800

    优化MultiRadioGroup代码

--- WidgetLibrary/src/main/java/org/caojun/widget/MultiRadioGroup.java
+++ WidgetLibrary/src/main/java/org/caojun/widget/MultiRadioGroup.java
@@ -1,388 +1,125 @@
 package org.caojun.widget;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityNodeInfo;
-import android.widget.CompoundButton;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.RadioGroup;
 
-public class MultiRadioGroup extends LinearLayout {
+public class MultiRadioGroup extends RadioGroup {
 
-    // holds the checked id; the selection is empty by default
-    private int mCheckedId = -1;
-    // tracks children radio buttons checked state
-    private CompoundButton.OnCheckedChangeListener mChildOnCheckedChangeListener;
-    // when true, mOnCheckedChangeListener discards events
-    private boolean mProtectFromCheckedChange = false;
-    private OnCheckedChangeListener mOnCheckedChangeListener;
-    private PassThroughHierarchyChangeListener mPassThroughListener;
-
-    /**
-     * {@inheritDoc}
-     */
     public MultiRadioGroup(Context context) {
         super(context);
-        setOrientation(VERTICAL);
-        init();
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public MultiRadioGroup(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
-    }
-
-    private void init() {
-        mChildOnCheckedChangeListener = new CheckedStateTracker();
-        mPassThroughListener = new PassThroughHierarchyChangeListener();
-        super.setOnHierarchyChangeListener(mPassThroughListener);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setOnHierarchyChangeListener(OnHierarchyChangeListener listener) {
-        // the user listener is delegated to our pass-through listener
-        mPassThroughListener.mOnHierarchyChangeListener = listener;
-    }
-
-    /**
-     * set the default checked radio button, without notification the listeners
-     * @param id the default checked radio button's id, if none use -1
-     */
-    public void setCheckWithoutNotif(int id){
-        if (id != -1 && (id == mCheckedId)) {
-            return;
-        }
-
-        mProtectFromCheckedChange = true;
-        if (mCheckedId != -1) {
-            setCheckedStateForView(mCheckedId, false);
-        }
-
-        if (id != -1) {
-            setCheckedStateForView(id, true);
-        }
-
-        mCheckedId = id;
-        mProtectFromCheckedChange = false;
     }
 
     @Override
-    public void addView(View child, int index, ViewGroup.LayoutParams params) {
-        List<RadioButton> btns = getAllRadioButton(child);
-        if(btns != null && btns.size() > 0){
-            for(RadioButton button : btns){
-                if (button.isChecked()) {
-                    mProtectFromCheckedChange = true;
-                    if (mCheckedId != -1) {
-                        setCheckedStateForView(mCheckedId, false);
-                    }
-                    mProtectFromCheckedChange = false;
-                    setCheckedId(button.getId());
-                }
-            }
-        }
-        super.addView(child, index, params);
-    }
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
-    /**
-     * get all radio buttons which are in the view
-     * @param child
-     */
-    private List<RadioButton> getAllRadioButton(View child){
-        List<RadioButton> btns = new ArrayList<>();
-        if (child instanceof RadioButton) {
-            btns.add((RadioButton) child);
-        }else if(child instanceof ViewGroup){
-            int counts = ((ViewGroup) child).getChildCount();
-            for(int i = 0; i < counts; i++){
-                btns.addAll(getAllRadioButton(((ViewGroup) child).getChildAt(i)));
-            }
-        }
-        return btns;
-    }
+        //调用ViewGroup的方法，测量子view
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
 
-    /**
-     * <p>Sets the selection to the radio button whose identifier is passed in
-     * parameter. Using -1 as the selection identifier clears the selection;
-     * such an operation is equivalent to invoking {@link #clearCheck()}.</p>
-     *
-     * @param id the unique id of the radio button to select in this group
-     *
-     * @see #getCheckedRadioButtonId()
-     * @see #clearCheck()
-     */
-    public void check(int id) {
-        // don't even bother
-        if (id != -1 && (id == mCheckedId)) {
-            return;
-        }
+        //最大的宽
+        int maxWidth = 0;
+        //累计的高
+        int totalHeight = 0;
 
-        if (mCheckedId != -1) {
-            setCheckedStateForView(mCheckedId, false);
-        }
+        //当前这一行的累计行宽
+        int lineWidth = 0;
+        //当前这行的最大行高
+        int maxLineHeight = 0;
+        //用于记录换行前的行宽和行高
+        int oldHeight;
+        int oldWidth;
 
-        if (id != -1) {
-            setCheckedStateForView(id, true);
-        }
+        int count = getChildCount();
+        //假设 widthMode和heightMode都是AT_MOST
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            MarginLayoutParams params = (MarginLayoutParams) child.getLayoutParams();
+            //得到这一行的最高
+            oldHeight = maxLineHeight;
+            //当前最大宽度
+            oldWidth = maxWidth;
 
-        setCheckedId(id);
-    }
+            int deltaX = child.getMeasuredWidth() + params.leftMargin + params.rightMargin;
+            if (lineWidth + deltaX + getPaddingLeft() + getPaddingRight() > widthSize) {//如果折行,height增加
+                //和目前最大的宽度比较,得到最宽。不能加上当前的child的宽,所以用的是oldWidth
+                maxWidth = Math.max(lineWidth, oldWidth);
+                //重置宽度
+                lineWidth = deltaX;
+                //累加高度
+                totalHeight += oldHeight;
+                //重置行高,当前这个View，属于下一行，因此当前最大行高为这个child的高度加上margin
+                maxLineHeight = child.getMeasuredHeight() + params.topMargin + params.bottomMargin;
 
-    private void setCheckedId(int id) {
-        mCheckedId = id;
-        if (mOnCheckedChangeListener != null) {
-            mOnCheckedChangeListener.onCheckedChanged(this, mCheckedId);
-        }
-    }
-
-    private void setCheckedStateForView(int viewId, boolean checked) {
-        View checkedView = findViewById(viewId);
-        if (checkedView != null && checkedView instanceof RadioButton) {
-            ((RadioButton) checkedView).setChecked(checked);
-        }
-    }
-
-    /**
-     * <p>Returns the identifier of the selected radio button in this group.
-     * Upon empty selection, the returned value is -1.</p>
-     *
-     * @return the unique id of the selected radio button in this group
-     *
-     * @see #check(int)
-     * @see #clearCheck()
-     *
-     * @attr ref android.R.styleable#MyRadioGroup_checkedButton
-     */
-    public int getCheckedRadioButtonId() {
-        return mCheckedId;
-    }
-
-    /**
-     * <p>Clears the selection. When the selection is cleared, no radio button
-     * in this group is selected and {@link #getCheckedRadioButtonId()} returns
-     * null.</p>
-     *
-     * @see #check(int)
-     * @see #getCheckedRadioButtonId()
-     */
-    public void clearCheck() {
-        check(-1);
-    }
-
-    /**
-     * <p>Register a callback to be invoked when the checked radio button
-     * changes in this group.</p>
-     *
-     * @param listener the callback to call on checked state change
-     */
-    public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
-        mOnCheckedChangeListener = listener;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public LayoutParams generateLayoutParams(AttributeSet attrs) {
-        return new MultiRadioGroup.LayoutParams(getContext(), attrs);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
-        return p instanceof MultiRadioGroup.LayoutParams;
-    }
-
-    @Override
-    protected LinearLayout.LayoutParams generateDefaultLayoutParams() {
-        return new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-    }
-
-    @Override
-    public void onInitializeAccessibilityEvent(AccessibilityEvent event) {
-        super.onInitializeAccessibilityEvent(event);
-        event.setClassName(MultiRadioGroup.class.getName());
-    }
-
-    @Override
-    public void onInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info) {
-        super.onInitializeAccessibilityNodeInfo(info);
-        info.setClassName(MultiRadioGroup.class.getName());
-    }
-
-    /**
-     * <p>This set of layout parameters defaults the width and the height of
-     * the children to {@link #WRAP_CONTENT} when they are not specified in the
-     * XML file. Otherwise, this class ussed the value read from the XML file.</p>
-     *
-     * <p>See
-     * for a list of all child view attributes that this class supports.</p>
-     *
-     */
-    public static class LayoutParams extends LinearLayout.LayoutParams {
-        /**
-         * {@inheritDoc}
-         */
-        public LayoutParams(Context c, AttributeSet attrs) {
-            super(c, attrs);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        public LayoutParams(int w, int h) {
-            super(w, h);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        public LayoutParams(int w, int h, float initWeight) {
-            super(w, h, initWeight);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        public LayoutParams(ViewGroup.LayoutParams p) {
-            super(p);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        public LayoutParams(MarginLayoutParams source) {
-            super(source);
-        }
-
-        /**
-         * <p>Fixes the child's width to
-         * {@link android.view.ViewGroup.LayoutParams#WRAP_CONTENT} and the child's
-         * height to  {@link android.view.ViewGroup.LayoutParams#WRAP_CONTENT}
-         * when not specified in the XML file.</p>
-         *
-         * @param a the styled attributes set
-         * @param widthAttr the width attribute to fetch
-         * @param heightAttr the height attribute to fetch
-         */
-        @Override
-        protected void setBaseAttributes(TypedArray a,
-                                         int widthAttr, int heightAttr) {
-
-            if (a.hasValue(widthAttr)) {
-                width = a.getLayoutDimension(widthAttr, "layout_width");
             } else {
-                width = WRAP_CONTENT;
+                //不换行，累加宽度
+                lineWidth += deltaX;
+                //不换行，计算行最高
+                int deltaY = child.getMeasuredHeight() + params.topMargin + params.bottomMargin;
+                maxLineHeight = Math.max(maxLineHeight, deltaY);
             }
-
-            if (a.hasValue(heightAttr)) {
-                height = a.getLayoutDimension(heightAttr, "layout_height");
-            } else {
-                height = WRAP_CONTENT;
+            if (i == count - 1) {
+                //前面没有加上下一行的搞，如果是最后一行，还要再叠加上最后一行的最高的值
+                totalHeight += maxLineHeight;
+                //计算最后一行和前面的最宽的一行比较
+                maxWidth = Math.max(lineWidth, oldWidth);
             }
         }
+
+        //加上当前容器的padding值
+        maxWidth += getPaddingLeft() + getPaddingRight();
+        totalHeight += getPaddingTop() + getPaddingBottom();
+        setMeasuredDimension(widthMode == MeasureSpec.EXACTLY ? widthSize : maxWidth,
+                heightMode == MeasureSpec.EXACTLY ? heightSize : totalHeight);
+
     }
 
-    /**
-     * <p>Interface definition for a callback to be invoked when the checked
-     * radio button changed in this group.</p>
-     */
-    public interface OnCheckedChangeListener {
-        /**
-         * <p>Called when the checked radio button has changed. When the
-         * selection is cleared, checkedId is -1.</p>
-         *
-         * @param group the group in which the checked radio button has changed
-         * @param checkedId the unique identifier of the newly checked radio button
-         */
-        public void onCheckedChanged(MultiRadioGroup group, int checkedId);
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int count = getChildCount();
+        //pre为前面所有的child的相加后的位置
+        int preLeft = getPaddingLeft();
+        int preTop = getPaddingTop();
+        //记录每一行的最高值
+        int maxHeight = 0;
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            MarginLayoutParams params = (MarginLayoutParams) child.getLayoutParams();
+            //r-l为当前容器的宽度。如果子view的累积宽度大于容器宽度，就换行。
+            if (preLeft + params.leftMargin + child.getMeasuredWidth() + params.rightMargin + getPaddingRight() > (r - l)) {
+                //重置
+                preLeft = getPaddingLeft();
+                //要选择child的height最大的作为设置
+                preTop = preTop + maxHeight;
+                maxHeight = getChildAt(i).getMeasuredHeight() + params.topMargin + params.bottomMargin;
+            } else { //不换行,计算最大高度
+                maxHeight = Math.max(maxHeight, child.getMeasuredHeight() + params.topMargin + params.bottomMargin);
+            }
+            //left坐标
+            int left = preLeft + params.leftMargin;
+            //top坐标
+            int top = preTop + params.topMargin;
+            int right = left + child.getMeasuredWidth();
+            int bottom = top + child.getMeasuredHeight();
+            //为子view布局
+            child.layout(left, top, right, bottom);
+            //计算布局结束后，preLeft的值
+            preLeft += params.leftMargin + child.getMeasuredWidth() + params.rightMargin;
+
+        }
+
     }
 
-    private class CheckedStateTracker implements CompoundButton.OnCheckedChangeListener {
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            // prevents from infinite recursion
-            if (mProtectFromCheckedChange) {
-                return;
-            }
-
-            mProtectFromCheckedChange = true;
-            if (mCheckedId != -1) {
-                setCheckedStateForView(mCheckedId, false);
-            }
-            mProtectFromCheckedChange = false;
-
-            int id = buttonView.getId();
-            setCheckedId(id);
-        }
-    }
-
-    /**
-     * <p>A pass-through listener acts upon the events and dispatches them
-     * to another listener. This allows the table layout to set its own internal
-     * hierarchy change listener without preventing the user to setup his.</p>
-     */
-    private class PassThroughHierarchyChangeListener implements
-            ViewGroup.OnHierarchyChangeListener {
-        private ViewGroup.OnHierarchyChangeListener mOnHierarchyChangeListener;
-
-        /**
-         * {@inheritDoc}
-         */
-        @SuppressLint("NewApi")
-        public void onChildViewAdded(View parent, View child) {
-            if (parent == MultiRadioGroup.this ) {
-                List<RadioButton> btns = getAllRadioButton(child);
-                if(btns != null && btns.size() > 0){
-                    for(RadioButton btn : btns){
-                        int id = btn.getId();
-                        // generates an id if it's missing
-                        if (id == View.NO_ID && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                            id = View.generateViewId();
-                            btn.setId(id);
-                        }
-                        btn.setOnCheckedChangeListener(
-                                mChildOnCheckedChangeListener);
-                    }
-                }
-            }
-
-            if (mOnHierarchyChangeListener != null) {
-                mOnHierarchyChangeListener.onChildViewAdded(parent, child);
-            }
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        public void onChildViewRemoved(View parent, View child) {
-            if (parent == MultiRadioGroup.this) {
-                List<RadioButton> btns = getAllRadioButton(child);
-                if(btns != null && btns.size() > 0){
-                    for(RadioButton btn : btns){
-                        btn.setOnCheckedChangeListener(null);
-                    }
-                }
-            }
-
-            if (mOnHierarchyChangeListener != null) {
-                mOnHierarchyChangeListener.onChildViewRemoved(parent, child);
-            }
-        }
+    public RadioButton getChildRadioButtonAt(int index) {
+        return (RadioButton) getChildAt(index);
     }
 }
